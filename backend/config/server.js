8280b3a17d0aa35452b8281ed5a3e33a004c53ad
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-02-05 18:09:34 +0900

    delete code

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'bd861f920560a6d8f9a534de738d2646'),
-    },
-  },
-});
--- memo.md
+++
@@ -1,5 +0,0 @@
-# メモ
-
-## nvm を使って node のバージョンを切り替える
-
-`https://dev-yakuza.posstree.com/environment/nvm/#node%E3%81%AE%E3%83%90%E3%83%BC%E3%82%B8%E3%83%A7%E3%83%B3%E3%83%AA%E3%82%B9%E3%83%88`
